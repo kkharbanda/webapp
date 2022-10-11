fce1dfc19c6b76ed3ec97be1f83a13a0b56fca4e
komalkharbanda <kkharbanda76@gmail.com>
2022-10-11 17:50:57 +0530

    login jwt authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+const User = require('../../models/Users');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require("express-validator");
 router.post(
   "/",
@@ -14,8 +19,54 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send("Welcome User panel");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      const avatar = 
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+    } catch (err) {
+      console.error(err.message);
+     return res.status(500).send('Server error');
+    }
   }
 );
 
