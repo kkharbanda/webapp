635d87e204d3f25f8479ac5f724421baf603e1e9
komalkharbanda <kkharbanda76@gmail.com>
2022-10-17 12:06:11 +0530

    get profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', function (req, res) {
-    res.send('Welcome Profile panel');
-});
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+const auth = require('../../middleware/auth');
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: req.user.id
+      }).populate('user', ['name', 'avatar']);
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
 module.exports= router;
