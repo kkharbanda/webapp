576b9064ddfd0bb1589f54d9346cf5840a897f60
komalkharbanda <kkharbanda76@gmail.com>
2022-10-18 12:34:29 +0530

    add profile and update of user

--- models/User.js
+++ models/User.js
@@ -19,4 +19,4 @@ const userSchema = new mongoose.Schema({
         default:Date.now
     }
 })
-module.exports= User = mongoose.model('User', userSchema);
+module.exports=  mongoose.model('user', userSchema);
