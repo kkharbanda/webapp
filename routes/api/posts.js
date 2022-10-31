e11570c0a2791ab5aae52054206ef2f4ba730e77
komalkharbanda <kkharbanda76@gmail.com>
2022-10-31 17:29:03 +0530

    unlike post changes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -121,4 +121,31 @@ router.put('/like/:id', auth, async (req, res) => {
       res.status(500).send('Server Error');
     }
   });
+  // @route    PUT api/posts/unlike/:id
+// @desc     Unlike a post
+// @access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has not yet been liked
+    if (!post.likes.some((like) => like.user.toString() === req.user.id)) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // remove the like
+    // post.likes = post.likes.filter(
+    //   ({ user }) => user.toString() !== req.user.id
+    // );
+const removeindex = post.likes.map(like =>like.user.toString()).indexOf(req.user.id);
+post.likes.splice(removeindex,1);
+    await post.save();
+
+    return res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports= router;
