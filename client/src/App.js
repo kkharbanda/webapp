5b7c0c6010563574c3f69a8411014c860527e622
komalkharbanda <kkharbanda76@gmail.com>
2022-11-11 14:57:37 +0530

    store alert

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import { Provider } from 'react-redux';
+import store from './store';
 import "./App.css";
 
 export default function App() {
   return (
+    <Provider store={store}>
     <Router>
       
-      <div>
+      
       <Navbar/>
         <Routes>
           <Route path="/" element={<Landing />} />
@@ -18,7 +21,8 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
-      </div>
+      
     </Router>
+    </Provider>
   );
 }
