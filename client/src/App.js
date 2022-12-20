d88cd91df3e79458a5803e69bec2609f097e8f98
komalkharbanda <kkharbanda76@gmail.com>
2022-12-20 17:50:53 +0530

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React , { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import { Provider } from "react-redux";
-import Alert from "./components/layout/Alert";
-import store from "./store";
-import "./App.css";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-forms/profileForm';
+import AddExperience from './components/profile-forms/AddExperince';
+import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
+import PrivateRoute from './components/routing/PrivateRoute';
+import { LOGOUT } from './actions/types';
+
+
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import Dashboard from "./components/dashboard/Dashboard";
-import createProfile from "./components/profile-forms/profileForm"
-import AddExperince from "./components/profile-forms/AddExperince";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/profile";
-import Posts from "./components/post/Posts";
-import { LOGOUT } from './actions/types';
-export default function App() {
-  
+
+import './App.css';
+
+const App = () => {
   useEffect(() => {
     // check for token in LS when app first runs
     if (localStorage.token) {
@@ -36,44 +43,45 @@ export default function App() {
       if (!localStorage.token) store.dispatch({ type: LOGOUT });
     });
   }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
-      
         <Alert />
         <Routes>
           <Route path="/" element={<Landing />} />
-          {/* 👇️ handle dynamic path */}
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           <Route path="profiles" element={<Profiles />} />
           <Route path="profile/:id" element={<Profile />} />
-        
           <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
           />
-           <Route
+          <Route
             path="create-profile"
-            element={<PrivateRoute component={createProfile} />}
+            element={<PrivateRoute component={ProfileForm} />}
           />
           <Route
             path="edit-profile"
-            element={<PrivateRoute component={createProfile} />}
+            element={<PrivateRoute component={ProfileForm} />}
           />
           <Route
-            path="/add-experience"
-            element={<PrivateRoute component={AddExperince} />}
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
           />
           <Route
-            path="/add-education"
+            path="add-education"
             element={<PrivateRoute component={AddEducation} />}
           />
           <Route path="posts" element={<PrivateRoute component={Posts} />} />
+          <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
+        
         </Routes>
-       
       </Router>
     </Provider>
   );
-}
+};
+
+export default App;
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import formatDate from '../../utils/formatDate';
 import { connect } from 'react-redux';
-
+import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
-  
+  addLike,
+  removeLike,
+  deletePost,
   auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
-
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  showActions
 }) => (
   <div className="post bg-white p-1 my-1">
     <div>
@@ -22,10 +24,10 @@ const PostItem = ({
       <p className="my-1">{text}</p>
       <p className="post-date">Posted on {formatDate(date)}</p>
 
-      
+      {showActions && (
         <Fragment>
           <button
-        
+            onClick={() => addLike(_id)}
             type="button"
             className="btn btn-light"
           >
@@ -33,7 +35,7 @@ const PostItem = ({
             <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
           <button
-           
+            onClick={() => removeLike(_id)}
             type="button"
             className="btn btn-light"
           >
@@ -47,7 +49,7 @@ const PostItem = ({
           </Link>
           {!auth.loading && user === auth.user._id && (
             <button
-        
+              onClick={() => deletePost(_id)}
               type="button"
               className="btn btn-danger"
             >
@@ -55,7 +57,7 @@ const PostItem = ({
             </button>
           )}
         </Fragment>
-     
+      )}
     </div>
   </div>
 );
@@ -66,13 +68,17 @@ PostItem.defaultProps = {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
- 
+  auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
+  showActions: PropTypes.bool
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, {  })(
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
   PostItem
 );
